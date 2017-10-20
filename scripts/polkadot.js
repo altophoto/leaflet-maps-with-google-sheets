e029b38749149b5fd9ca6b7350f88dbbc0bf735a
PolkadotRodeo <sebnoah@gmail.com>
2017-10-20 19:49:43 -0400

    Update polkadot.js

--- scripts/polkadot.js
+++ scripts/polkadot.js
@@ -16,7 +16,8 @@ var showPolygons = function() {
   };
 
 // group.addTo(map)
-function onClick (marker, travelOptions) {
+$('#map').on('click', '.trigger', function(marker, travelOptions) {
+// function onClick (marker, travelOptions) {
   var defaultColors = r360.config.defaultTravelTimeControlOptions.travelTimes.filter(function(t){
     return defaultTravelTimes.indexOf(t.time) > -1;
   }).map(function(t){
