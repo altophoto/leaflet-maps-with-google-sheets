6d2a3f479da2deca5caa8dca195f1abe1f6cf1e6
PolkadotRodeo <sebnoah@gmail.com>
2017-10-20 20:22:44 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description'] + ('<h2><a href="#"><button class="trigger">' + 'isochrone' + '</button></a></h2>'))
+          point['Description'] + ("<h2><a href='#'><button class='trigger'>" + 'isochrone' + "</button></a></h2>")
           .addEventListener('click', onClick.bind(null, marker));
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
