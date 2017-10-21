6f72f013b5321df3b53e1515500d9b3ce0bcaa59
PolkadotRodeo <sebnoah@gmail.com>
2017-10-21 10:20:39 -0400

    Update polkadot.js

--- scripts/polkadot.js
+++ scripts/polkadot.js
@@ -9,7 +9,7 @@ polygonLayer.setColors([
      // { 'time': 1500, 'opacity': 1.0, 'color': '#C1272D' },
      // ["#509373", "#75c380", "#abcf79"]
 ])
-polygonLayer.setStrokeWidth(9);
+// polygonLayer.setStrokeWidth(9);
 
 $('#map').on('click', '.trigger', function onClick (marker, travelOptions) {
   var polygonLayer = r360.leafletPolygonLayer()
